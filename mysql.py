81d19250e44da96890e369b1dacca4b59a0396e8
moscosky <eren_cizme@hotmail.com>
2015-07-14 15:52:53 +0200

    update-final

--- mysql.py
+++ mysql.py
@@ -47,7 +47,7 @@ def insertReading(tagId):
 def insertCard(tagId):
     db = connect()
     cur = db.cursor()
-    cur.execute("""INSERT INTO cards (name, tagId, active) VALUES ('new', %s, 0)""",(tagId)
+    cur.execute("""INSERT INTO cards (name, tagId, active) VALUES ('new', %s, 0)""",(tagId))
     db.commit()
     db.close()
 
